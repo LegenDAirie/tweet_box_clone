dee66e93b7aa3c3792030c851b3fd76a1b318599
Airiel D. Salvatore <airiel.d.salvatore@gmail.com>
2016-08-19 20:21:33 -0700

    changed some stuff

--- src/image_input.js
+++ src/image_input.js
@@ -1,43 +1,18 @@
 import React from 'react';
-import first from 'lodash/first';
 
-var ImageInput = React.createClass({
 
-  getInitialState: function() {
-    return {
-      file: '',
-      imagePreview: ''
-    };
-  },
+var ImageInput = function(props) {
 
-  uploadHandler: function(event) {
-    event.preventDefault();
+  var { uploadHandler } = props;
 
-    var reader = new window.FileReader();
-    var file = first(event.target.files);
-
-    reader.onloadend = function() {
-      this.setState({
-        file: file,
-        imagePreview: reader.result
-      });
-    }.bind(this);
-
-    reader.readAsDataURL(file);
-  },
-
-  render: function() {
-
-    return (
-      <div>
-        <input type='file'
-        accept='image/gif,image/jpeg,image/jpg,image/png'
-        onChange={this.uploadHandler}
-        />
-        <img src={this.state.imagePreview} alt="stuff"/>
-      </div>
-    );
-  }
-});
+  return (
+    <div>
+      <input type='file'
+      accept='image/gif,image/jpeg,image/jpg,image/png'
+      onChange={uploadHandler}
+      />
+    </div>
+  );
+};
 
 export default ImageInput;
--- src/tweet_box.js
+++ src/tweet_box.js
@@ -2,6 +2,8 @@ import React from 'react';
 import inRange from 'lodash/inRange';
 import CharacterCount from './character_count';
 import ImageInput from './image_input';
+import first from 'lodash/first';
+
 
 var TweetBox = React.createClass({
 
@@ -9,6 +11,8 @@ var TweetBox = React.createClass({
 
     return {
       text: '',
+      file: '',
+      imagePreview: ''
     };
   },
 
@@ -17,16 +21,52 @@ var TweetBox = React.createClass({
     this.setState({text: event.target.value});
   },
 
+  uploadHandler: function(event) {
+    // event.preventDefault();
+
+    var reader = new window.FileReader();
+    var file = first(event.target.files);
+
+    reader.onloadend = function() {
+      this.setState({
+        file: file,
+        imagePreview: reader.result
+      });
+    }.bind(this);
+
+    reader.readAsDataURL(file);
+  },
+
   render: function() {
     var { text } = this.state;
     var invalidNumberOfCharacters = !inRange(text.length, 1, 141);
 
+    // <div contentEditable='true' role='textbox' placeholder="What's happening?">
+    // </div>
     return (
-      <div>
-        <input placeholder="type here!" onChange={this.handleInputChange}/>
-        <button disabled={invalidNumberOfCharacters}>submit!</button>
-        <CharacterCount textLength={text.length}/>
-        <ImageInput/>
+      <div className='tweet-box-container'>
+
+        <div className='title-container'>
+          <h2>Compose new Tweet</h2>
+        </div>
+
+        <div className='tweet-form-container'>
+
+          <div className='tweet-content'>
+            <div contentEditable='true'>
+            </div>
+            <img src={this.state.imagePreview} alt=""/>
+            <textarea placeholder="type here!" onChange={this.handleInputChange}/>
+          </div>
+
+          <div className='tool-bar'>
+            <ImageInput uploadHandler={this.uploadHandler}/>
+            <CharacterCount textLength={text.length}/>
+            <button disabled={invalidNumberOfCharacters}>submit!</button>
+          </div>
+
+        </div>
+
       </div>
     );
   }
